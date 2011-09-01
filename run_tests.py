747144988fcb5012cc76fa221c6d5d66cc444cb2
Clemens Anhuth <blackwell@nexgo.de>
2011-09-01 18:02:15 +0200

    Add tests for ERRORLEVEL behavior

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,11 @@
 import unittest
-from tests import common_tests, completion_tests
+from tests import common_tests, completion_tests, pycmd_tests
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(common_tests.suite())
     suite.addTest(completion_tests.suite())
+    suite.addTest(pycmd_tests.suite())
     return suite
 
 if __name__ == '__main__':
