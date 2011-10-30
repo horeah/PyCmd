dd74fc202383225c29769bf36d07d248c0991204
Horea Haitonic <horeah@gmail.com>
2011-10-30 18:09:35 +0200

    Added unit tests for the color manipulation functions

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,12 @@
 import unittest
-from tests import common_tests, completion_tests, pycmd_tests
+from tests import common_tests, completion_tests, pycmd_tests, console_tests
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(common_tests.suite())
     suite.addTest(completion_tests.suite())
     suite.addTest(pycmd_tests.suite())
+    suite.addTest(console_tests.suite())
     return suite
 
 if __name__ == '__main__':
---
+++ tests/console_tests.py
@@ -0,0 +1,110 @@
+#
+# Unit tests for console.py
+#
+
+from unittest import TestCase, TestSuite, defaultTestLoader
+import console
+from console import write_str, get_text_attributes, set_text_attributes
+from pycmd_public import color
+
+class TestColors(TestCase):
+    """Test the manipulation of color attributes"""
+
+    def setUp(self):
+        self.orig_attr = get_text_attributes()
+        
+    def tearDown(self):
+        set_text_attributes(self.orig_attr)
+
+    def testRgbSet(self):
+        """Test the setting of individual RGB components"""
+        write_str(color.Fore.SET_RED 
+                  + color.Fore.SET_GREEN 
+                  + color.Fore.SET_BLUE
+                  + color.Fore.SET_BRIGHT)
+        attr = get_text_attributes()
+        self.assertTrue(attr & console.FOREGROUND_RED 
+                        and attr & console.FOREGROUND_GREEN 
+                        and attr & console.FOREGROUND_BLUE
+                        and attr & console.FOREGROUND_BRIGHT)
+
+    def testRgbClear(self):
+        """Test the clearing of individual RGB components"""
+        write_str(color.Fore.CLEAR_RED 
+                  + color.Fore.CLEAR_GREEN 
+                  + color.Fore.CLEAR_BLUE
+                  + color.Fore.CLEAR_BRIGHT)
+        attr = get_text_attributes()
+        self.assertFalse(attr & console.FOREGROUND_RED 
+                         or attr & console.FOREGROUND_GREEN 
+                         or attr & console.FOREGROUND_BLUE
+                         or attr & console.FOREGROUND_BRIGHT)
+
+    def testRgbToggle(self):
+        """Test the toggling of individual RGB components"""
+        attr = get_text_attributes()
+        set_text_attributes(attr 
+                            | console.FOREGROUND_RED 
+                            | console.FOREGROUND_GREEN
+                            | console.FOREGROUND_BLUE
+                            | console.FOREGROUND_BRIGHT)
+        write_str(color.Fore.TOGGLE_RED 
+                  + color.Fore.TOGGLE_GREEN 
+                  + color.Fore.TOGGLE_BLUE
+                  + color.Fore.TOGGLE_BRIGHT)
+        attr = get_text_attributes()
+        self.assertFalse(attr & console.FOREGROUND_RED 
+                         or attr & console.FOREGROUND_GREEN 
+                         or attr & console.FOREGROUND_BLUE
+                         or attr & console.FOREGROUND_BRIGHT)
+
+    def testNamedColors(self):
+        """Test the predefined colors (named combinations of RGB components)"""
+        write_str(color.Fore.RED)
+        attr = get_text_attributes()
+        self.assertTrue(attr & console.FOREGROUND_RED
+                        and not attr & console.FOREGROUND_GREEN
+                        and not attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.GREEN)
+        attr = get_text_attributes()
+        self.assertTrue(not attr & console.FOREGROUND_RED
+                        and attr & console.FOREGROUND_GREEN
+                        and not attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.YELLOW)
+        attr = get_text_attributes()
+        self.assertTrue(attr & console.FOREGROUND_RED
+                        and attr & console.FOREGROUND_GREEN
+                        and not attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.BLUE)
+        attr = get_text_attributes()
+        self.assertTrue(not attr & console.FOREGROUND_RED
+                        and not attr & console.FOREGROUND_GREEN
+                        and attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.MAGENTA)
+        attr = get_text_attributes()
+        self.assertTrue(attr & console.FOREGROUND_RED
+                        and not attr & console.FOREGROUND_GREEN
+                        and attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.CYAN)
+        attr = get_text_attributes()
+        self.assertTrue(not attr & console.FOREGROUND_RED
+                        and attr & console.FOREGROUND_GREEN
+                        and attr & console.FOREGROUND_BLUE)
+
+        write_str(color.Fore.WHITE)
+        attr = get_text_attributes()
+        self.assertTrue(attr & console.FOREGROUND_RED
+                        and attr & console.FOREGROUND_GREEN
+                        and attr & console.FOREGROUND_BLUE)
+
+
+def suite():
+    suite = TestSuite()
+    suite.addTest(defaultTestLoader.loadTestsFromTestCase(TestColors))
+    return suite
+
