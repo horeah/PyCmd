614a97c2f3f227362f9b9e76350a1dde57aed950
Horea Haitonic <horeah@gmail.com>
2011-11-23 19:37:58 +0200

    Revert "Add tests for ERRORLEVEL behavior"
    This reverts commit 747144988fcb5012cc76fa221c6d5d66cc444cb2.
    
    See Issue #3388191 for details on why this is happening

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,10 @@
 import unittest
-from tests import common_tests, completion_tests, pycmd_tests, console_tests
+from tests import common_tests, completion_tests, console_tests
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(common_tests.suite())
     suite.addTest(completion_tests.suite())
-    suite.addTest(pycmd_tests.suite())
     suite.addTest(console_tests.suite())
     return suite
 
