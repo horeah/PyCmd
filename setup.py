7836189463478729f8bd427958d27de4226966a1
Horea Haitonic <horeah@gmail.com>
2016-12-06 11:19:17 +0200

    Update cx_Freeze setup to new version

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name = 'PyCmd',
     version = '0.8',
     description = 'Smart windows shell',
-    executables = [Executable('PyCmd.py')],
+    executables = [Executable(script='PyCmd.py',
+                              icon='PyCmd.ico')],
     options = {
         'build_exe': {
-          'icon': 'PyCmd.ico',
           'include_files': ['example-init.py',
                             'pycmd_public.html',
                             (dirname(lib2to3.__file__), 'lib2to3')],
