horeah/PyCmd

commit a839e2cd98d8102d386d1145636ac9ff4d12405a

Optimize listing completions (should be twice as fast now)
The printing loop was unnecessarily recomputing some things for each
completion; they have now been moved out of the loop.

PyCmd.py
@@ -444,6 +444,17 @@ def main():
                                 if not ord(rec.Char) in [0, 8, 13, 27]:
                                     state.handle(ActionCode.ACTION_INSERT, rec.Char)
                                 continue
+
+                        if has_wildcards(tokens[-1]):
+                            # Substring matching wildcards will be printed in a different color
+                            path_sep = '/' if '/' in expand_env_vars(tokens[-1]) else '\\'
+                            tokens = parse_line(completed.rstrip(path_sep))
+                            token = tokens[-1].replace('"', '')
+                            (_, _, prefix) = token.rpartition(path_sep)
+                            match = wildcard_to_regex(prefix + '*').match(s)
+                        else:
+                            # Length of the common prefix will be printed in a different color
+                            common_prefix_len = len(find_common_prefix(state.before_cursor, suggestions))
                             
                         stdout.write('\n')
                         for line in range(0, num_lines):
@@ -454,11 +465,6 @@ def main():
                                     s = suggestions[line + column * num_lines]
                                     if has_wildcards(tokens[-1]):
                                         # Print wildcard matches in a different color
-                                        path_sep = '/' if '/' in expand_env_vars(tokens[-1]) else '\\'
-                                        tokens = parse_line(completed.rstrip(path_sep))
-                                        token = tokens[-1].replace('"', '')
-                                        (_, _, prefix) = token.rpartition(path_sep)
-                                        match = wildcard_to_regex(prefix + '*').match(s)
                                         current_index = 0
                                         for i in range(1, match.lastindex + 1):
                                             stdout.write(color.Fore.DEFAULT + color.Back.DEFAULT +
@@ -470,7 +476,6 @@ def main():
                                         stdout.write(color.Fore.DEFAULT + color.Back.DEFAULT + ' ' * (column_width - len(s)))
                                     else:
                                         # Print the common part in a different color
-                                        common_prefix_len = len(find_common_prefix(state.before_cursor, suggestions))
                                         stdout.write(color.Fore.DEFAULT + color.Back.DEFAULT +
                                                      appearance.colors.completion_match +
                                                      s[:common_prefix_len] +

commit 990a8d036fd3c2da20714323422c9a10557f65d6

Completion printing optimization broke wildcard highlighting
The optimization introduced in a839e2cd98d8102d386d1145636ac9ff4d12405a was too
aggressive and broke some cases where wildcards were involved.

This fixes the case without almost no loss to the gained speed.

PyCmd.py
@@ -451,7 +451,7 @@ def main():
                             tokens = parse_line(completed.rstrip(path_sep))
                             token = tokens[-1].replace('"', '')
                             (_, _, prefix) = token.rpartition(path_sep)
-                            match = wildcard_to_regex(prefix + '*').match(s)
+                            pattern = wildcard_to_regex(prefix + '*')
                         else:
                             # Length of the common prefix will be printed in a different color
                             common_prefix_len = len(find_common_prefix(state.before_cursor, suggestions))
@@ -465,6 +465,7 @@ def main():
                                     s = suggestions[line + column * num_lines]
                                     if has_wildcards(tokens[-1]):
                                         # Print wildcard matches in a different color
+                                        match = pattern.match(s)
                                         current_index = 0
                                         for i in range(1, match.lastindex + 1):
                                             stdout.write(color.Fore.DEFAULT + color.Back.DEFAULT +