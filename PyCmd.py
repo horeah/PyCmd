280c8ba9578f0440e0664995c6dba5b0ed8dbeb4
Horea Haitonic <horeah@gmail.com>
2023-03-10 11:57:55 +0200

    Linux: support additional keys (Alt-/, PgUp/Dn, Ctrl-G)

--- PyCmd.py
+++ PyCmd.py
@@ -381,7 +381,7 @@ def main():
                     state.handle(ActionCode.ACTION_DELETE_WORD)
                 elif rec.VirtualKeyCode == 8:           # Alt-Backspace
                     state.handle(ActionCode.ACTION_BACKSPACE_WORD)
-                elif rec.VirtualKeyCode == 191:
+                elif rec.VirtualKeyCode == 191:         # Alt-/
                     state.handle(ActionCode.ACTION_EXPAND)
             elif is_ctrl_pressed(rec) and is_alt_pressed(rec):          # Ctrl-Alt-something
                 if rec.VirtualKeyCode == 75:            # Ctrl-Alt-K
